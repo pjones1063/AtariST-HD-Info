765957497bedecb50239c66f7ea2b8b29d08338f
paul <paul@jns.lan>
2019-07-29 10:04:54 -0400

    add MegaMax LC exmaple 3

--- DISKINFM.C
+++ DISKINFM.C
@@ -4,7 +4,7 @@
 #include "DISKINFO.H"
 
 /*******************************************************/
-/*   DISKINFO.C  MAGAMAX Laser C                       */
+/*   DISKINFO.C  MEGAMAX Laser C                       */
 /*                                                     */
 /*   Simple program to demo Atari 16 bit RSC files     */
 /*                                                     */
@@ -39,11 +39,11 @@ struct diskinfo {
 
 /* disk info array */
 struct diskinfos {
-    char fre[10],		 
+    char fre[10],
          cpd[10],
-	     prc[10],
-	     bps[10],
-	     spc[10];
+         prc[10],
+         bps[10],
+         spc[10];
 };
 
 /* Global variables */
@@ -57,7 +57,7 @@ int i;
 char * value;
 {
    TEDINFO *ob_tedinfo;
-   ob_tedinfo = (TEDINFO *) dialog[i].ob_spec;	
+   ob_tedinfo = (TEDINFO *) dialog[i].ob_spec;
    ob_tedinfo -> te_ptext = value;
    return 1;
 }
@@ -73,12 +73,12 @@ int disk;
    setText(spcc[disk], dis->spc);
    setText(bpsc[disk], dis->bps);
    return 1;
-}	
+}
 
 /* get info for one hard disk */
 struct diskinfos getDiskInfo(disk)
 int disk; 
-{	
+{
 	struct diskinfo di;
     struct diskinfos dis;
 	char temp_str[10];
@@ -87,36 +87,36 @@ int disk;
     { 
 	 /* getdfree(disk, &di); */
      Dfree(&di,disk); 
-  	 sprintf (dis.fre,"%ld", (di.fre * di.bps * di.spc) / 1024);	
-	 sprintf (dis.cpd,"%ld", (di.cpd * di.bps * di.spc) / 1024);	
-    
+  	 sprintf (dis.fre,"%ld", (di.fre * di.bps * di.spc) / 1024);
+	 sprintf (dis.cpd,"%ld", (di.cpd * di.bps * di.spc) / 1024);
+
      percent = (di.cpd * di.bps * di.spc) - (di.fre * di.bps * di.spc);
-	 percent = (percent*100) / (di.cpd * di.bps * di.spc); 			 
+	 percent = (percent*100) / (di.cpd * di.bps * di.spc); 
      sprintf (dis.prc,"%ld", percent );	
-	
+
 	 sprintf (dis.bps,"%ld", di.bps);
-	 sprintf (dis.spc,"%ld", di.spc);		
+	 sprintf (dis.spc,"%ld", di.spc);
     } 
     else 
     {
-     strcpy (dis.fre,  "  ");	
+     strcpy (dis.fre,  "  ");
      strcpy (dis.cpd,  "  ");
      strcpy (dis.prc,  "  ");
-     strcpy (dis.bps,  "  ");	
-     strcpy (dis.spc,  "  ");	
-	}		  
+     strcpy (dis.bps,  "  ");
+     strcpy (dis.spc,  "  ");
+	}
     return dis;
 }
 
 
 /*main program */
 int main()
-{	
-	struct diskinfos dinfos[16];	
-	short handle, junk;		
+{
+	struct diskinfos dinfos[16];
+	short handle, junk;
     int ch, i, rez, d;
 	char temp_str[20];
-	
+
 	appl_init();		/* start AES */
 	handle=graf_handle(&junk, &junk, &junk, &junk);	/* find AES handle */
 	for (i=0; i<10; work_in[i++] = 1);
@@ -124,39 +124,39 @@ int main()
 	v_opnvwk(work_in, &handle, work_out);			/* open workstation */
         rez = Getrez();	
 	if(rez == 0)
-	{	
+	{
 	  form_alert (1,"[1][Med/High Rez only][I am done]");
-	}	
+	}
 	else if (!rsrc_load ("DISKINFO.RSC")) 
-	{ 
+	{
            form_alert (1,"[1][DISKINFO.RSC is missing][All done]");
 	}
 	else
 	{
           rsrc_gaddr(R_TREE, DG1, &dialog) ; 
           form_center (dialog, &x, &y,  &w , &h) ;
-	     /*  drive_map = setdisk (0); */ 	  
+	     /*  drive_map = setdisk (0); */
 	         drive_map = Dsetdrv(0);
-           
+
 	  for(d=0; d < 13; d++) {       /* get info on 13 hd disks */
-	      dinfos[d] = getDiskInfo(d+3);  
+	      dinfos[d] = getDiskInfo(d+3); 
 	      setDiskInfo(&dinfos[d], d);
 	   }
-	  
+
 	      graf_mouse (FINGER,&junk);    /* finish up the display */
-          form_dial (FMD_START , 0, 0, 10, 10, x, y, w, h) ;
+              form_dial (FMD_START , 0, 0, 10, 10, x, y, w, h) ;
 	      form_dial (FMD_GROW , 0, 0, 10, 10, x, y, w, h) ;
-          objc_draw (dialog, 0, 10, x, y, w, h) ;
-	  
+              objc_draw (dialog, 0, 10, x, y, w, h) ;
+
       do {							/* loop intil exit click*/
           ch = form_do (dialog, DG1) ; 
 	  } while (ch != BT1);
-	  
-	      form_dial (FMD_SHRINK , 0, 0, 10, 10, x, y, w, h) ;
-          form_dial (FMD_FINISH , 0, 0, 10, 10, x, y, w, h) ;
+
+             form_dial (FMD_SHRINK , 0, 0, 10, 10, x, y, w, h) ;
+             form_dial (FMD_FINISH , 0, 0, 10, 10, x, y, w, h) ;
     }
-	
-    v_clsvwk(handle);				/* close workstation */
+
+        v_clsvwk(handle);				/* close workstation */
 	appl_exit();					/* shutdown AES */
 	return 0;
 }
--- DISKINFO.C
+++ DISKINFO.C
@@ -44,7 +44,7 @@ struct diskinfo {
 
 /* disk info array */
 struct diskinfos {
-    char    free[10],		 
+    char    free[10],
             cpd[10],
 	    prc[10],
 	    bps[10],
@@ -73,11 +73,11 @@ int setDiskInfo(struct diskinfos * dis, int disk) {
    setText(spcc[disk], dis->spc);
    setText(bpsc[disk], dis->bps);
    return 1;
-}	
+}
 
 /* get info for one hard disk */
 struct diskinfos getDiskInfo(int disk) 
-{	
+{
 	struct diskinfo di;
         struct diskinfos dis;
 	char temp_str[10];
@@ -86,35 +86,35 @@ struct diskinfos getDiskInfo(int disk)
     { 
 	 getdfree(disk, &di); 
   	 sprintf (dis.free,"%ld", (di.free * di.bps * di.spc) / 1024);	
-	 sprintf (dis.cpd,"%ld", (di.cpd * di.bps * di.spc) / 1024);	
-    
+	 sprintf (dis.cpd,"%ld", (di.cpd * di.bps * di.spc) / 1024);
+ 
          percent = (di.cpd * di.bps * di.spc) - (di.free * di.bps * di.spc);
-	 percent = (percent*100) / (di.cpd * di.bps * di.spc); 			 
-         sprintf (dis.prc,"%ld", percent );	
-	
+	 percent = (percent*100) / (di.cpd * di.bps * di.spc);
+         sprintf (dis.prc,"%ld", percent );
+
 	 sprintf (dis.bps,"%ld", di.bps);
-	 sprintf (dis.spc,"%ld", di.spc);		
+	 sprintf (dis.spc,"%ld", di.spc);
     } 
     else 
     {
-     strcpy (dis.free, "  ");	
+     strcpy (dis.free, "  ");
      strcpy (dis.cpd,  "  ");
      strcpy (dis.prc,  "  ");
-     strcpy (dis.bps,  "  ");	
-     strcpy (dis.spc,  "  ");	
-	}		  
+     strcpy (dis.bps,  "  ");
+     strcpy (dis.spc,  "  ");
+	}
     return dis;
 }
 
 
 /*main program */
 int main(void)
-{	
-	struct diskinfos dinfos[16];	
-	short handle, junk;		
+{
+	struct diskinfos dinfos[16];
+	short handle, junk;
         int ch, i, rez, d;
 	char temp_str[20];
-	
+
 	appl_init();		/* start AES */
 	handle=graf_handle(&junk, &junk, &junk, &junk);	/* find AES handle */
 	for (i=0; i<10; work_in[i++] = 1);
@@ -122,9 +122,9 @@ int main(void)
 	v_opnvwk(work_in, &handle, work_out);			/* open workstation */
         rez = Getrez();	
 	if(rez == 0)
-	{	
+	{
 	  form_alert (1,"[1][Med/High Rez only][I am done]");
-	}	
+	}
 	else if (!rsrc_load ("DISKINFO.RSC")) 
 	{ 
            form_alert (1,"[1][DISKINFO.RSC is missing][All done]");
@@ -133,26 +133,26 @@ int main(void)
 	{
           rsrc_gaddr(R_TREE, DG1, &dialog) ; 
           form_center (dialog, &x, &y,  &w , &h) ;
-	  drive_map = setdisk (0); 	  
-	  
+	  drive_map = setdisk (0);
+
 	  for(d=0; d < 13; d++) {       /* get info on 13 hd disks */
-	      dinfos[d] = getDiskInfo(d+3);  
+	      dinfos[d] = getDiskInfo(d+3); 
 	      setDiskInfo(&dinfos[d], d);
 	   }
-	  
+ 
 	  graf_mouse (FINGER,&junk);    /* finish up the display */
           form_dial (FMD_START , 0, 0, 10, 10, x, y, w, h) ;
 	  form_dial (FMD_GROW , 0, 0, 10, 10, x, y, w, h) ;
           objc_draw (dialog, 0, 10, x, y, w, h) ;
-	  
+ 
       do {							/* loop intil exit click*/
           ch = form_do (dialog, DG1) ; 
 	  } while (ch != BT1);
-	  
+ 
 	  form_dial (FMD_SHRINK , 0, 0, 10, 10, x, y, w, h) ;
           form_dial (FMD_FINISH , 0, 0, 10, 10, x, y, w, h) ;
     }
-	
+
         v_clsvwk(handle);				/* close workstation */
 	appl_exit();					/* shutdown AES */
 	return 0;
